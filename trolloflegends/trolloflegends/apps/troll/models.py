b22fc4cec0d692d80d5ad5adb9e403974cafe16a
Shavakan <chiyah92@gmail.com>
2014-06-20 19:55:55 +0900

    models field validated

--- trolloflegends/trolloflegends/apps/troll/models.py
+++ trolloflegends/trolloflegends/apps/troll/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.modles import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Trolling(models.Model):
@@ -7,15 +7,15 @@ class Trolling(models.Model):
     content = models.TextField()
     written_time = models.DateTimeField()
     num_votes = models.IntegerField()
-    voters = models.ManyToManyField(User, related_names="voted_trollings")
-    latitude = models.DecimalField()
-    longitude = models.DecimalField()
-    image = models.ImageField()
+    voters = models.ManyToManyField(User, related_name="voted_trollings")
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    image = models.ImageField(upload_to="images")
 
 class Report(models.Model):
     user = models.ForeignKey(User, related_name="reports")
     content = models.TextField()
     written_time = models.DateTimeField()
     num_votes = models.IntegerField()
-    voters = models.ManyToManyField(User, related_names="voted_reports")
+    voters = models.ManyToManyField(User, related_name="voted_reports")
 
