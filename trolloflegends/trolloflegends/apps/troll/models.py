5f346f82e140db223fad15e671680b826c31fdc8
Shavakan <chiyah92@gmail.com>
2014-06-20 19:16:09 +0900

    Initialized models.py schema

--- trolloflegends/trolloflegends/apps/troll/models.py
+++ trolloflegends/trolloflegends/apps/troll/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.modles import User
 
 # Create your models here.
+class Trolling(models.Model):
+    user = models.ForeignKey(User, related_name="trollings")
+    content = models.TextField()
+    written_time = models.DateTimeField()
+    num_votes = models.IntegerField()
+    voters = models.ManyToManyField(User, related_names="voted_trollings")
+    latitude = models.DecimalField()
+    longitude = models.DecimalField()
+    image = models.ImageField()
+
+class Report(models.Model):
+    user = models.ForeignKey(User, related_name="reports")
+    content = models.TextField()
+    written_time = models.DateTimeField()
+    num_votes = models.IntegerField()
+    voters = models.ManyToManyField(User, related_names="voted_reports")
+
